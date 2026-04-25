734b48a165b3ee2dd3385ea64748683233411138
Zhang Linli <zhangmagle125@163.com>
2026-04-25 21:32:09 +0800

    gradient descent

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,6 +1,11 @@
+import copy
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
-from lab_utils_common import plot_data, sigmoid, dlc
+from lab_utils_common import plot_data, sigmoid, dlc #, plt_tumor_data, compute_cost_logistic)
+#from plt_quad_logistic import plt_quad_logistic, plt_prob
+plt.style.use('./deeplearning.mplstyle')
 
 X_train = np.array([[0.5, 1.5], [1, 1],[1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
 y_train = np.array([0, 0, 0, 1, 1, 1])
@@ -28,3 +33,54 @@ def compute_cost_logistic(X, y, w, b):
 w_tmp = np.array([1,1])
 b_tmp = -3
 print(compute_cost_logistic(X_train, y_train, w_tmp, b_tmp))
+
+def compute_gradient_logistic(X, y, w, b):
+    m,n = X.shape
+    dj_dw = np.zeros((n, ))
+    dj_db = 0.
+
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i], w) + b)
+        err_i = f_wb_i - y[i]
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i, j]
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m
+    dj_db = dj_db/m
+
+    return dj_db, dj_dw
+
+
+X_tmp = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y_tmp = np.array([0, 0, 0, 1, 1, 1])
+w_tmp = np.array([2.,3.])
+b_tmp = 1.
+dj_db_tmp, dj_dw_tmp = compute_gradient_logistic(X_tmp, y_tmp, w_tmp, b_tmp)
+print(f"dj_db: {dj_db_tmp}" )
+print(f"dj_dw: {dj_dw_tmp.tolist()}" )
+
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters):
+    j_history = []
+    w = copy.deepcopy(w_in)
+    b = b_in
+
+    for i in range(num_iters):
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
+        if i < 100000:
+            j_history.append(compute_cost_logistic(X, y, w, b))
+
+        if i % math.ceil(num_iters/10) == 0:
+            print(f"iteration {i:4d}: Cost {j_history[-1]}")
+
+    return w, b, j_history
+
+w_tmp  = np.zeros_like(X_train[0])
+b_tmp  = 0.
+alph = 0.1
+iters = 10000
+
+w_out, b_out, _ = gradient_descent(X_train, y_train, w_tmp, b_tmp, alph, iters)
+print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
